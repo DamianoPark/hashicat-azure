b5b0a4c55dd8677c01022daa200e778609aa95b4
DaminaoPark <jaewoo201@gmail.com>
2022-10-26 07:44:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,13 +2,5 @@ module "network" {
   source  = "app.terraform.io/JaewooPark-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-}
-
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
-
-  tags = {
-    environment = "Production"
-  }
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
