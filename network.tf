cbb23f844a2f2d9eb3e01de940720f8613227af6
DaminaoPark <jaewoo201@gmail.com>
2022-10-26 07:41:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -3,3 +3,12 @@ module "network" {
   version = "3.5.0"
   # insert required variables here
 }
+
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
